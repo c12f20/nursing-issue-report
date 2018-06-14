0aa66c1cbf7765c3a60275ccf58d797e0a440000
Hill <hill@HilltekiMacBook-Pro.local>
2018-06-14 21:18:06 +0800

    Add both 32bit/64bit windows app release process Add two missing directories

--- dist-win.js
+++ dist-win.js
@@ -3,9 +3,12 @@
 const electronInstaller = require('electron-winstaller');
 const path = require('path');
 
+const args = process.argv.splice(2);
+const arch = args[0]
+
 electronInstaller.createWindowsInstaller({
-    appDirectory: path.resolve(__dirname, 'build/NIR-win32-x64'),
-    outputDirectory: path.resolve(__dirname, 'dist/installer64'),
+    appDirectory: path.resolve(__dirname, `build/NIR-win32-${arch}`),
+    outputDirectory: path.resolve(__dirname, `dist/installer-${arch}`),
     authors: 'hill.cen',
     exe: "NIR.exe",
     description: "护理不良事件报告工具",
